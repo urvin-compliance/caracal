546fdf31176368ea3726b6cf9a0bbd203b39f2dc
John Dugan <john@plia.com>
2014-08-11 13:05:48 -0500

    Adjusted default styles based on sample document I just created.

--- lib/caracal/core/styles.rb
+++ lib/caracal/core/styles.rb
@@ -18,15 +18,15 @@ module Caracal
           
           def self.default_styles
             [
-              { id: 'Normal',   name: 'normal',    font: 'Arial',    size: 20, spacing: 276, color: '333333' },
-              { id: 'Heading1', name: 'heading 1', font: 'Palatino', size: 32, top: 200 },
-              { id: 'Heading2', name: 'heading 2', font: 'Palatino', size: 26, top: 200, bold: true },
-              { id: 'Heading3', name: 'heading 3', font: 'Palatino', size: 24, top: 160, color: '666666', bold: true },
-              { id: 'Heading4', name: 'heading 4', font: 'Palatino', size: 22, top: 160, color: '666666', underline: true },
-              { id: 'Heading5', name: 'heading 5', font: 'Palatino', size: 22, top: 160, color: '666666' },
-              { id: 'Heading6', name: 'heading 6', font: 'Palatino', size: 22, top: 160, color: '666666', italic: true },
+              { id: 'Normal',   name: 'normal',    font: 'Arial',    size: 20, spacing: 360, color: '333333' },
+              { id: 'Heading1', name: 'heading 1', font: 'Palatino', size: 36, bottom: 120 },
+              { id: 'Heading2', name: 'heading 2', font: 'Arial',    size: 26, top: 120, bottom: 160, bold: true },
+              { id: 'Heading3', name: 'heading 3', font: 'Arial',    size: 24, top: 120, bottom: 160, bold: true, italic: true, color: '666666' },
+              { id: 'Heading4', name: 'heading 4', font: 'Palatino', size: 24, top: 120, bottom: 120, bold: true },
+              { id: 'Heading5', name: 'heading 5', font: 'Arial',    size: 22, top: 120, bottom: 120, bold: true },
+              { id: 'Heading6', name: 'heading 6', font: 'Arial',    size: 22, top: 120, bottom: 120, underline: true, italic: true, color: '666666' },
               { id: 'Title',    name: 'title',     font: 'Palatino', size: 60 },
-              { id: 'Subtitle', name: 'subtitle',  font: 'Arial',    size: 28, bottom: 200, color: '666666', italic: true }
+              { id: 'Subtitle', name: 'subtitle',  font: 'Arial',    size: 28, top: 60 }
             ]           
           end
           
