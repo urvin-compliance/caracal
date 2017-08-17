a325e461269a6040bd932d36b69afa1fddc02f7e
John Dugan <jdugan@users.noreply.github.com>
2017-08-17 19:19:03 +0200

    Added line to numbering xml to tell word all numbering definitions are multiline hybrids.

--- lib/caracal/renderers/numbering_renderer.rb
+++ lib/caracal/renderers/numbering_renderer.rb
@@ -8,21 +8,22 @@ require 'caracal/renderers/xml_renderer'
 module Caracal
   module Renderers
     class NumberingRenderer < XmlRenderer
-      
+
       #-------------------------------------------------------------
       # Public Methods
       #-------------------------------------------------------------
-      
-      # This method produces the xml required for the `word/numbering.xml` 
+
+      # This method produces the xml required for the `word/numbering.xml`
       # sub-document.
       #
       def to_xml
         builder = ::Nokogiri::XML::Builder.with(declaration_xml) do |xml|
           xml.send 'w:numbering', root_options do
-            
+
             # add abstract definitions
             document.toplevel_lists.each_with_index do |model, i|
               xml.send 'w:abstractNum', { 'w:abstractNumId' => i + 1 } do
+                xml.send 'w:multiLevelType', { 'w:val' => 'hybridMultilevel' }
                 model.level_map.each do |(level, type)|
                   if s = document.find_list_style(type, level)
                     xml.send 'w:lvl', { 'w:ilvl' => s.style_level } do
@@ -42,7 +43,7 @@ module Caracal
                 end
               end
             end
-            
+
             # bind individual tables to abstract definitions
             document.toplevel_lists.each_with_index do |model, i|
               xml.send 'w:num', { 'w:numId' => i + 1 } do
@@ -50,18 +51,18 @@ module Caracal
               end
             end
           end
-          
+
         end
         builder.to_xml(save_options)
       end
-      
-      
-      
+
+
+
       #-------------------------------------------------------------
       # Private Methods
-      #------------------------------------------------------------- 
+      #-------------------------------------------------------------
       private
-      
+
       def root_options
         {
           'xmlns:mc'  => 'http://schemas.openxmlformats.org/markup-compatibility/2006',
@@ -81,7 +82,7 @@ module Caracal
           'xmlns:dgm' => 'http://schemas.openxmlformats.org/drawingml/2006/diagram'
         }
       end
-   
+
     end
   end
 end
