64ff95f7caff451f48fbb4e5b27885903d5e8bdc
Victor Polkó <victorpolko@gmail.com>
2021-08-06 14:42:55 +0300

    01 Add a failing test-case

--- spec/lib/caracal/core/images_spec.rb
+++ spec/lib/caracal/core/images_spec.rb
@@ -13,11 +13,24 @@ describe Caracal::Core::Images do
     # .img
     describe '.img' do
       let!(:size) { subject.contents.size }
-      
-      before { subject.img 'https://www.google.com/images/srpr/logo11w.png', width: 538, height: 190 }
-      
-      it { expect(subject.contents.size).to eq size + 1 }
-      it { expect(subject.contents.last).to be_a(Caracal::Core::Models::ImageModel) }
+      let!(:url) { 'https://www.google.com/images/srpr/logo11w.png' }
+
+      describe 'when no data provided' do
+        before { subject.img url, width: 538, height: 190 }
+
+        it { expect(subject.contents.size).to eq size + 1 }
+        it { expect(subject.contents.last).to be_a(Caracal::Core::Models::ImageModel) }
+      end
+
+      describe 'when data provided as binary' do
+        let!(:binary_data) { open(url).read }
+
+        before { subject.img url, data: binary_data, width: 538, height: 190 }
+
+        it { expect(subject.contents.size).to eq size + 1 }
+        it { expect(subject.contents.last).to be_a(Caracal::Core::Models::ImageModel) }
+        it { expect(subject.contents.last.image_data).to eq binary_data }
+      end
     end
     
   end
